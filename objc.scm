0e53f438bd70e161cced28a8661eb7a92fa8fad4
Jason Felice <jason.m.felice@gmail.com>
2012-11-30 21:33:40 -0500

    Round trip selectors

--- objc.scm
+++ objc.scm
@@ -1,11 +1,25 @@
 (c-declare "#include <objc/objc-runtime.h>")
 
+;; Working with Classes
+(define objc.class_getName
+  (c-lambda ((type "Class" (Class id)))
+	    char-string
+    "___result = (char*) class_getName(___arg1);"))
+
+;; Obtaining Class Definitions
 (define objc.objc_getClass
   (c-lambda (nonnull-char-string)
 	    (type "id" (id))
     "objc_getClass"))
 
-(define objc.class_getName
-  (c-lambda ((type "Class" (Class id)))
+;; Working with Selectors
+(define objc.sel_getUid
+  (c-lambda (nonnull-char-string)
+	    (type "SEL" (SEL))
+    "sel_getUid"))
+
+(define objc.sel_getName
+  (c-lambda ((type "SEL" (SEL)))
 	    char-string
-    "___result = (char*) class_getName(___arg1);"))
+    "___result = (char*) sel_getName(___arg1);"))
+
--- test.scm
+++ test.scm
@@ -7,3 +7,4 @@
        (raise 'expr)))))
 
 (test (string=? (objc.class_getName (objc.objc_getClass "NSString")) "NSString"))
+(test (string=? (objc.sel_getName (objc.sel_getUid "stringByAppendingString:")) "stringByAppendingString:"))
