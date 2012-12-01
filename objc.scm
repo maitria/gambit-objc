969ba47028ed2a4f126db0cc36a09807ba7729b2
Jason Felice <jason.m.felice@gmail.com>
2012-11-30 21:53:20 -0500

    Extract C types

--- objc.scm
+++ objc.scm
@@ -1,25 +1,29 @@
 (c-declare "#include <objc/objc-runtime.h>")
 
+(c-define-type objc.id (type "id" (id)))
+(c-define-type objc.Class (type "Class" (Class id)))
+(c-define-type objc.SEL (type "SEL" (SEL)))
+
 ;; Working with Classes
 (define objc.class_getName
-  (c-lambda ((type "Class" (Class id)))
+  (c-lambda (objc.Class)
 	    char-string
     "___result = (char*) class_getName(___arg1);"))
 
 ;; Obtaining Class Definitions
 (define objc.objc_getClass
   (c-lambda (nonnull-char-string)
-	    (type "id" (id))
+	    objc.id
     "objc_getClass"))
 
 ;; Working with Selectors
 (define objc.sel_getUid
   (c-lambda (nonnull-char-string)
-	    (type "SEL" (SEL))
+	    objc.SEL
     "sel_getUid"))
 
 (define objc.sel_getName
-  (c-lambda ((type "SEL" (SEL)))
+  (c-lambda (objc.SEL)
 	    char-string
     "___result = (char*) sel_getName(___arg1);"))
 
