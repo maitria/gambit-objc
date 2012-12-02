a42ec099bb4c6f4f0a066c880f69ea6649c9b8d4
Jason Felice <jason.m.felice@gmail.com>
2012-12-02 10:23:21 -0500

    NSObject is a class

--- test.scm
+++ test.scm
@@ -7,6 +7,7 @@
        (raise 'expr)))))
 
 (test (not (objc.Class? 42)))
+(test (objc.Class? (objc.objc_getClass "NSObject")))
 
 (test (string=? (objc.class_getName (objc.objc_getClass "NSString")) "NSString"))
 (test (string=? (objc.sel_getName (objc.sel_getUid "stringByAppendingString:")) "stringByAppendingString:"))
