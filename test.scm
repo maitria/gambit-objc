d8e2f8da490ed6390d847629e2b78d1016d4625b
Jason Felice <jason.m.felice@gmail.com>
2012-12-02 10:24:06 -0500

    formatting

--- test.scm
+++ test.scm
@@ -6,10 +6,11 @@
      (if (not expr)
        (raise 'expr)))))
 
+;; Classes
 (test (not (objc.Class? 42)))
 (test (objc.Class? (objc.objc_getClass "NSObject")))
-
 (test (string=? (objc.class_getName (objc.objc_getClass "NSString")) "NSString"))
+
 (test (string=? (objc.sel_getName (objc.sel_getUid "stringByAppendingString:")) "stringByAppendingString:"))
 
 
