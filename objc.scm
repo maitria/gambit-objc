00aa9d5fa77c3719422cf5572d7ce202966f2365
Jason Felice <jason.m.felice@gmail.com>
2012-12-02 16:24:07 -0500

    Use method_copyArgumentType() instead of method_getArgumentType()

--- objc.scm
+++ objc.scm
@@ -74,9 +74,7 @@ EOF
   (c-lambda (objc.Method unsigned-int)
 	    char-string
 #<<EOF
-  char* argument_type_buffer = (char*)malloc(256);
-  method_getArgumentType(___arg1, ___arg2, argument_type_buffer, 256);
-  argument_type_buffer[255] = '\0';
+  char* argument_type_buffer = method_copyArgumentType(___arg1, ___arg2);
   ___result = argument_type_buffer;
 #define ___AT_END free(argument_type_buffer);
 EOF
