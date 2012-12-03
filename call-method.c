59ce50897c3cdb96ade9f82d71b8ce139c9347a2
Jason Felice <jason.m.felice@gmail.com>
2012-12-03 14:13:37 -0500

    Inline sel_getUid("UTF8String")

--- call-method.c
+++ call-method.c
@@ -1,6 +1,5 @@
 
 static Class NSString;
-static SEL UTF8String;
 
 static void             call_method_init(void);
 static ___SCMOBJ        call_method(id object, SEL sel, ___SCMOBJ args);
@@ -9,7 +8,6 @@ static ___SCMOBJ        id_to_SCMOBJ(id result);
 static void call_method_init(void)
 {
         NSString = (Class)objc_getClass("NSString");
-        UTF8String = sel_getUid("UTF8String");
 }
 
 static ___SCMOBJ call_method(id object, SEL sel, ___SCMOBJ args)
@@ -24,7 +22,7 @@ static ___SCMOBJ id_to_SCMOBJ(id result)
         if ((BOOL)objc_msgSend(result, sel_getUid("isKindOfClass:"), NSString)) {
                 ___SCMOBJ str = ___NUL;
                 ___SCMOBJ err = ___FIX(___NO_ERR);
-                char *charp = (char*)objc_msgSend(result, UTF8String);
+                char *charp = (char*)objc_msgSend(result, sel_getUid("UTF8String"));
                 err = ___EXT(___CHARSTRING_to_SCMOBJ) (charp, &str, -1);
                 if (err != ___FIX(___NO_ERR))
                         return ___FIX(___UNKNOWN_ERR);
