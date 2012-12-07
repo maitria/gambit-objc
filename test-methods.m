3e7b3834cd6fe4eededb868f6cbc2ae2455e678d
Jason Felice <jason.m.felice@gmail.com>
2012-12-06 22:05:59 -0500

    Compact test-methods.m

--- test-methods.m
+++ test-methods.m
@@ -5,64 +5,18 @@
 
 @implementation TestMethods
 
-+ (BOOL)methodReturningYES
-{
-	return YES;
-}
-
-+ (BOOL)methodReturningNO
-{
-	return NO;
-}
-
-+ (void)voidMethod
-{
-}
-
-+ (int)methodReturningInt42
-{
-	return 42;
-}
-
-+ (long)methodReturningLong43
-{
-	return 43L;
-}
-
-+ (short)methodReturningShort41
-{
-	return 41;
-}
-
-+ (unsigned short)methodReturningUnsignedShort40
-{
-	return 40;
-}
-
-+ (unsigned int)methodReturningUnsignedInt39
-{
-	return 39;
-}
-
-+ (unsigned long)methodReturningUnsignedLong99
-{
-	return 99UL;
-}
-
-+ (const char *)methodReturningCString
-{
-	return "a C string";
-}
-
-+ (NSObject *)methodReturningNSObject
-{
-	return [[[NSObject alloc] init] autorelease];
-}
-
-+ (NSObject *)methodReturningNil
-{
-	return nil;
-}
++ (BOOL)methodReturningYES { return YES; }
++ (BOOL)methodReturningNO { return NO; }
++ (void)voidMethod {}
++ (int)methodReturningInt42 { return 42; }
++ (long)methodReturningLong43 { return 43L; }
++ (short)methodReturningShort41 { return 41; }
++ (unsigned short)methodReturningUnsignedShort40 { return 40; }
++ (unsigned int)methodReturningUnsignedInt39 { return 39; }
++ (unsigned long)methodReturningUnsignedLong99 { return 99UL; }
++ (const char *)methodReturningCString { return "a C string"; }
++ (NSObject *)methodReturningNSObject { return [[[NSObject alloc] init] autorelease]; }
++ (NSObject *)methodReturningNil { return nil; }
 
 @end
 
