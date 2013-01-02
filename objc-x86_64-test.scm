d4ced3dcf91f7593b477fbe495816e18f4eac035
Jason Felice <jason.m.felice+jf@gmail.com>
2013-01-02 13:30:11 -0500

    x86_64 long long passed as 'INTEGER

--- objc-x86_64-test.scm
+++ objc-x86_64-test.scm
@@ -27,6 +27,7 @@
 (expect (eq? 'INTEGER (classify #\i)))
 (expect (eq? 'INTEGER (classify #\s)))
 (expect (eq? 'INTEGER (classify #\l)))
+(expect (eq? 'INTEGER (classify #\q)))
 (expect (eq? 'SSE (classify #\f)))
 
 (display-expect-results)
--- objc-x86_64.scm
+++ objc-x86_64.scm
@@ -4,7 +4,7 @@
 
 (define (classify objc-type-code)
   (case objc-type-code
-   ((#\c #\i #\l #\s) 'INTEGER)
+   ((#\c #\i #\l #\s #\q) 'INTEGER)
    ((#\f) 'SSE)))
 
 (define (sizeof objc-type-code)
