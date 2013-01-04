ccc8025b49c70b2d6e4303452aac8fb888ddd8a5
Jason Felice <jason.m.felice+jf@gmail.com>
2013-01-04 13:15:09 -0500

    Use strings for ObjC type codes

--- objc-x86_64-test.scm
+++ objc-x86_64-test.scm
@@ -1,9 +1,9 @@
 (import expect)
 (import objc-x86_64)
 
-(define *integral-types* '(#\c #\i #\s #\l #\q #\C #\I #\S #\L #\Q #\B))
-(define *pointer-types* '(#\@ #\# #\: #\^ #\?))
-(define *floating-point-types* '(#\f #\d))
+(define *integral-types* '("c" "i" "s" "l" "q" "C" "I" "S" "L" "Q" "B"))
+(define *pointer-types* '("@" "#" ":" "^" "?"))
+(define *floating-point-types* '("f" "d"))
 
 (define (expect-each-of type-code-list #!key to-be-classified-as to-have-size)
   (define (descriptive-message type-code clause thing)
@@ -30,10 +30,10 @@
     type-code-list))
 
 ;; Sizes of C types
-(expect-each-of '(#\B #\C #\c) to-have-size: 1)
-(expect-each-of '(#\S #\s) to-have-size: 2)
-(expect-each-of '(#\I #\L #\i #\l #\f) to-have-size: 4)
-(expect-each-of '(#\Q #\q #\d) to-have-size: 8)
+(expect-each-of '("B" "C" "c") to-have-size: 1)
+(expect-each-of '("S" "s") to-have-size: 2)
+(expect-each-of '("I" "L" "i" "l" "f") to-have-size: 4)
+(expect-each-of '("Q" "q" "d") to-have-size: 8)
 (expect-each-of *pointer-types* to-have-size: 8)
 
 ;; Classifying C types
--- objc-x86_64.scm
+++ objc-x86_64.scm
@@ -3,12 +3,12 @@
   sizeof)
 
 (define (classify objc-type-code)
-  (case objc-type-code
+  (case (string-ref objc-type-code 0)
    ((#\c #\i #\l #\s #\q #\C #\I #\S #\L #\Q #\B #\* #\@ #\# #\: #\^ #\?) 'INTEGER)
    ((#\f #\d) 'SSE)))
 
 (define (sizeof objc-type-code)
-  (case objc-type-code
+  (case (string-ref objc-type-code 0)
    ((#\c #\C #\B) 1)
    ((#\s #\S) 2)
    ((#\i #\l #\I #\L #\f) 4)
