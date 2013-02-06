1f2863245e17eedb96c4c8e410b942727fccfb79
Jason Felice <jason.m.felice+jf@gmail.com>
2013-02-06 09:34:49 -0500

    TRAMPOLINE-ALLOCATE will allocate gp registers

--- x86_64-allocate-test.scm
+++ x86_64-allocate-test.scm
@@ -2,6 +2,22 @@
 (import x86_64-allocate)
 (import x86_64-trampoline)
 
-(expect (not (raises? (lambda () (trampoline-allocate (make-trampoline) '())))))
+(expect "TRAMPOLINE-ALLOCATE with no parameters succeeds"
+  (not (raises? (lambda () (trampoline-allocate (make-trampoline) '())))))
+
+(expect "TRAMPOLINE-ALLOCATE can allocate general registers"
+  (let ((t (make-trampoline)))
+    (trampoline-allocate t '(((gp . 11))
+			     ((gp . 22))
+			     ((gp . 33))
+			     ((gp . 44))
+			     ((gp . 55))
+			     ((gp . 66))))
+    (expect (= 11 (trampoline-gp-ref t 0)))
+    (expect (= 22 (trampoline-gp-ref t 1)))
+    (expect (= 33 (trampoline-gp-ref t 2)))
+    (expect (= 44 (trampoline-gp-ref t 3)))
+    (expect (= 55 (trampoline-gp-ref t 4)))
+    (expect (= 66 (trampoline-gp-ref t 5)))))
 
 (display-expect-results)
--- x86_64-allocate.scm
+++ x86_64-allocate.scm
@@ -2,4 +2,12 @@
 (export trampoline-allocate)
 
 (define (trampoline-allocate trampoline parameters)
-  #f)
+  (let allocate ((remaining-parameters parameters)
+                 (next-gp 0))
+    (cond
+      ((null? remaining-parameters)
+       trampoline)
+      (else
+       (trampoline-gp-set! trampoline next-gp (cdaar remaining-parameters))
+       (allocate (cdr remaining-parameters) (+ 1 next-gp))))))
+
