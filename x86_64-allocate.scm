d2955d24f051812248a387758f08959017660714
Jason Felice <jason.m.felice+jf@gmail.com>
2013-02-06 15:01:37 -0500

    Extract PARAMETER

--- x86_64-allocate.scm
+++ x86_64-allocate.scm
@@ -5,15 +5,16 @@
   (let allocate ((remaining-parameters parameters)
                  (next-gp 0)
 		 (next-sse 0))
-    (cond
-      ((null? remaining-parameters)
-       trampoline)
-      ((eq? 'gp (caaar remaining-parameters))
-       (trampoline-gp-set! trampoline next-gp (cdaar remaining-parameters))
-       (allocate (cdr remaining-parameters) (+ 1 next-gp) next-sse))
-      ((eq? 'sse (caaar remaining-parameters))
-       (trampoline-sse-set! trampoline next-sse (cdaar remaining-parameters))
-       (allocate (cdr remaining-parameters) next-gp (+ 1 next-sse)))
-      (else
-       (raise "don't know how to handle")))))
+    (if (null? remaining-parameters)
+      trampoline
+      (let ((parameter (car remaining-parameters)))
+	(cond
+	  ((eq? 'gp (caar parameter))
+	   (trampoline-gp-set! trampoline next-gp (cdar parameter))
+	   (allocate (cdr remaining-parameters) (+ 1 next-gp) next-sse))
+	  ((eq? 'sse (caar parameter))
+	   (trampoline-sse-set! trampoline next-sse (cdar parameter))
+	   (allocate (cdr remaining-parameters) next-gp (+ 1 next-sse)))
+	  (else
+	   (raise "don't know how to handle")))))))
 
