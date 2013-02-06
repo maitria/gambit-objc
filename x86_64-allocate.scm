11b66d6de71997560546f3c7a972f08cb47022ae
Jason Felice <jason.m.felice+jf@gmail.com>
2013-02-06 15:32:16 -0500

    Refactor TRAMPOLINE-ALLOCATE

--- x86_64-allocate.scm
+++ x86_64-allocate.scm
@@ -2,23 +2,32 @@
 (export trampoline-allocate)
 
 (define (trampoline-allocate trampoline parameters)
-  (let allocate ((remaining-parameters parameters)
-                 (next-gp 0)
-		 (next-sse 0))
+  (define next-gp 0)
+  (define next-sse 0)
+
+  (define (store-gp value)
+    (trampoline-gp-set! trampoline next-gp value)
+    (set! next-gp (+ 1 next-gp)))
+
+  (define (store-sse value)
+    (trampoline-sse-set! trampoline next-sse value)
+    (set! next-sse (+ 1 next-sse)))
+
+  (let allocate ((remaining-parameters parameters))
     (if (null? remaining-parameters)
       #!void
-      (let word-loop ((parameter (car remaining-parameters))
-		      (next-gp next-gp)
-		      (next-sse next-sse))
+      (let word-loop ((parameter (car remaining-parameters)))
 	(if (null? parameter)
-	  (allocate (cdr remaining-parameters) next-gp next-sse)
+	  (allocate (cdr remaining-parameters))
 	  (cond
 	    ((eq? 'gp (caar parameter))
-	     (trampoline-gp-set! trampoline next-gp (cdar parameter))
-	     (word-loop (cdr parameter) (+ 1 next-gp) next-sse))
+	     (store-gp (cdar parameter))
+	     (word-loop (cdr parameter)))
+
 	    ((eq? 'sse (caar parameter))
-	     (trampoline-sse-set! trampoline next-sse (cdar parameter))
-	     (word-loop (cdr parameter) next-gp (+ 1 next-sse)))
+	     (store-sse (cdar parameter))
+	     (word-loop (cdr parameter)))
+
 	    (else
 	     (raise "don't know how to handle"))))))))
 
