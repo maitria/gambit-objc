f199794dc95a37074f00f69e5a66efe97c77d779
Jason Felice <jason.m.felice@gmail.com>
2013-03-17 19:57:30 -0400

    Extract the big C bits from objc.scm

---
+++ lib/objc-native.c
@@ -0,0 +1,264 @@
+#define OBJC2_UNAVAILABLE /* Avoid deprecation warnings */
+
+#import <objc/message.h>
+#import <CoreFoundation/CoreFoundation.h>
+#include <string.h>
+#include <stdlib.h>
+
+static ___SCMOBJ release_object(void *object)
+{
+  CFRelease(object);
+  return ___NUL;
+}
+
+static ___SCMOBJ take_object(id object, ___SCMOBJ *scm_result)
+{
+  if (!object) {
+    *scm_result = ___NUL;
+    return ___FIX(___NO_ERR);
+  }
+
+  CFRetain(object);
+  return ___EXT(___POINTER_to_SCMOBJ) (object, object_tags(), release_object, scm_result, -1);
+}
+
+typedef long parameter_word_t;
+
+#define MAX_PARAMETER_WORDS 16
+#define IMP_PARAMETERS \
+  (call->target, call->selector, \
+   call->parameter_words[0], call->parameter_words[1], call->parameter_words[2], \
+   call->parameter_words[3], call->parameter_words[4], call->parameter_words[5], \
+   call->parameter_words[6], call->parameter_words[7], call->parameter_words[8], \
+   call->parameter_words[9], call->parameter_words[10], call->parameter_words[11], \
+   call->parameter_words[12], call->parameter_words[13], call->parameter_words[14], \
+   call->parameter_words[15] \
+   )
+
+struct CLEAN_UP_THUNK_tag {
+  struct CLEAN_UP_THUNK_tag *next;
+  void *data;
+  void (* function) (void*);
+};
+
+typedef struct CLEAN_UP_THUNK_tag CLEAN_UP_THUNK;
+
+typedef struct {
+  id target;
+  SEL selector;
+  Class class;
+  Method method;
+  IMP imp;
+
+  parameter_word_t *current_word;
+  parameter_word_t parameter_words[MAX_PARAMETER_WORDS];
+
+  CLEAN_UP_THUNK *clean_up_thunks;
+} CALL;
+
+static const char *skip_qualifiers(const char *signature)
+{
+  static const char *IGNORABLE_METHOD_QUALIFIERS = "rnNoORV";
+  while (*signature && strchr(IGNORABLE_METHOD_QUALIFIERS, *signature))
+    ++signature;
+  return signature;
+}
+
+static char CALL_parameter_type(CALL *call, int parameter_number)
+{
+  char *signature = method_copyArgumentType(call->method, 2 + parameter_number);
+  if (!signature)
+    return '!';
+  char result = *skip_qualifiers(signature);
+  free(signature);
+  return result;
+}
+
+static void CALL_add_clean_up_thunk(CALL *call, void *data, void (* function) (void*))
+{
+  CLEAN_UP_THUNK *thunk = (CLEAN_UP_THUNK *)malloc(sizeof(CLEAN_UP_THUNK));
+  thunk->data = data;
+  thunk->function = function;
+  thunk->next = call->clean_up_thunks;
+  call->clean_up_thunks = thunk;
+}
+
+static void CALL_add_parameter_data(CALL *call, void* ptr, size_t size)
+{
+  int i;
+  if (size <= sizeof(parameter_word_t)) {
+    *call->current_word++ = *(parameter_word_t*)ptr;
+  } else {
+    for (i = 0; i < size; i += sizeof(parameter_word_t)) {
+      *call->current_word++ = ((parameter_word_t*)ptr)[i];
+    }
+  }
+}
+
+#define EASY_CONVERSION_CASE(_type,_c_type,_scm_typename) \
+  case _type: \
+    { \
+      _c_type value; \
+      err = ___EXT(___SCMOBJ_to_##_scm_typename) (arg, &value, -1); \
+      if (err == ___FIX(___NO_ERR)) \
+        CALL_add_parameter_data(call, &value, sizeof(_c_type)); \
+    } \
+    break;
+
+static ___SCMOBJ CALL_parse_parameters(CALL *call, ___SCMOBJ args)
+{
+  call->current_word = call->parameter_words;
+  int parameter_number = 0;
+  while (___PAIRP(args)) {
+    ___SCMOBJ arg = ___CAR(args);
+    ___SCMOBJ err = ___FIX(___NO_ERR);
+    switch (CALL_parameter_type(call, parameter_number)) {
+    EASY_CONVERSION_CASE('B',___BOOL,BOOL)
+    EASY_CONVERSION_CASE('c',___BOOL,BOOL)
+    EASY_CONVERSION_CASE('S',unsigned short,USHORT)
+    EASY_CONVERSION_CASE('s',short,SHORT)
+    EASY_CONVERSION_CASE('I',unsigned int,UINT)
+    EASY_CONVERSION_CASE('i',int,INT)
+    EASY_CONVERSION_CASE('L',unsigned long,ULONG)
+    EASY_CONVERSION_CASE('l',long,LONG)
+    EASY_CONVERSION_CASE('Q',unsigned long long,ULONGLONG)
+    EASY_CONVERSION_CASE('q',long long,LONGLONG)
+    EASY_CONVERSION_CASE('f',float,FLOAT)
+    EASY_CONVERSION_CASE('d',double,DOUBLE)
+    case '*':
+      {
+        char *value;
+        err = ___EXT(___SCMOBJ_to_CHARSTRING) (arg, &value, -1);
+        CALL_add_clean_up_thunk(call, value, ___release_string);
+        if (err == ___FIX(___NO_ERR)) \
+          CALL_add_parameter_data(call, &value, sizeof(char*));
+      }
+      break;
+    case ':':
+      {
+        if (!is_selector(arg))
+          return ___FIX(___UNKNOWN_ERR);
+        SEL sel_arg = ___CAST(SEL, ___CAST(void*,___FIELD(arg,___FOREIGN_PTR)));
+        CALL_add_parameter_data(call, &sel_arg, sizeof(SEL));
+      }
+      break;
+    case '#':
+    case '@':
+      {
+        if (!is_object(arg)) {
+          return ___FIX(___UNKNOWN_ERR);
+	}
+	id arg_as_id = (id)0;
+	err = ___EXT(___SCMOBJ_to_POINTER) (arg, (void **)&arg_as_id, object_tags(), -1);
+	if (err != ___FIX(___NO_ERR))
+	  return err;
+        CALL_add_parameter_data(call, &arg_as_id, sizeof(id));
+      }
+      break;
+    default:
+      fprintf(stderr, "Unhandled parameter type: %c\n", CALL_parameter_type(call, parameter_number));
+      err = ___FIX(___UNIMPL_ERR);
+      break;
+    }
+    if (err != ___FIX(___NO_ERR)) {
+      return err;
+    }
+    args = ___CDR(args);
+    ++parameter_number;
+  }
+  return ___FIX(___NO_ERR);
+}
+#undef EASY_CONVERSION_CASE
+
+static char CALL_return_type(CALL *call)
+{
+  return *skip_qualifiers(method_getTypeEncoding(call->method));
+}
+
+#define CALL_FOR_IMP_RESULT(_type,_result) \
+  _type _result = ((_type (*) (id,SEL,...))call->imp) IMP_PARAMETERS;
+#define EASY_CONVERSION_CASE(spec,name,c_type) \
+  case spec: \
+    { \
+      CALL_FOR_IMP_RESULT(c_type,objc_result) \
+      return ___EXT(___##name##_to_SCMOBJ) (objc_result, result, -1); \
+    }
+static ___SCMOBJ CALL_invoke(CALL *call, ___SCMOBJ *result)
+{
+  switch (CALL_return_type(call)) {
+  case 'c':
+  case 'B':
+    {
+      CALL_FOR_IMP_RESULT(BOOL,imp_result)
+      *result = imp_result ? ___TRU : ___FAL;
+      return ___FIX(___NO_ERR);
+    }
+  case 'v':
+    {
+      call->imp IMP_PARAMETERS;
+      *result = ___VOID;
+      return ___FIX(___NO_ERR);
+    }
+  case ':':
+    {
+      CALL_FOR_IMP_RESULT(SEL,sel_result)
+      return ___EXT(___POINTER_to_SCMOBJ) (sel_result, selector_tags(), NULL, result, -1);
+    }
+  case '@':
+  case '#':
+    {
+      CALL_FOR_IMP_RESULT(id,objc_result)
+      return take_object(objc_result, result);
+    }
+  EASY_CONVERSION_CASE('*',CHARSTRING,char*)
+  EASY_CONVERSION_CASE('f',FLOAT,float)
+  EASY_CONVERSION_CASE('d',DOUBLE,double)
+  EASY_CONVERSION_CASE('S',USHORT,unsigned short)
+  EASY_CONVERSION_CASE('s',SHORT,signed short)
+  EASY_CONVERSION_CASE('I',UINT,unsigned int)
+  EASY_CONVERSION_CASE('i',INT,signed int)
+  EASY_CONVERSION_CASE('L',ULONG,unsigned long)
+  EASY_CONVERSION_CASE('l',LONG,long)
+  EASY_CONVERSION_CASE('Q',ULONGLONG,unsigned long long)
+  EASY_CONVERSION_CASE('q',LONGLONG,signed long long)
+  }
+  fprintf(stderr, "UNKNOWN RETURN TYPE: %c\n", CALL_return_type(call));
+  return ___FIX(___UNIMPL_ERR);
+}
+#undef EASY_CONVERSION_CASE
+
+static void CALL_clean_up(CALL *call)
+{
+  CLEAN_UP_THUNK *thunk = call->clean_up_thunks, *next;
+  while (thunk) {
+    thunk->function(thunk->data);
+    next = thunk->next;
+    free(thunk);
+    thunk = next;
+  }
+}
+
+static ___SCMOBJ call_method(id target, SEL selector, ___SCMOBJ *result, ___SCMOBJ args)
+{
+  CALL call;
+
+  memset(&call, 0, sizeof(call));
+  call.target = target;
+  call.selector = selector;
+  call.class = (Class)object_getClass(call.target);
+  call.method = class_getInstanceMethod(call.class, call.selector);
+  if (!call.method) {
+    return ___FIX(___UNIMPL_ERR);
+  }
+  call.imp = method_getImplementation(call.method);
+
+  ___SCMOBJ err = CALL_parse_parameters(&call, args);
+  if (err != ___FIX(___NO_ERR)) {
+    return err;
+  }
+
+  err = CALL_invoke(&call, result);
+  CALL_clean_up(&call);
+  return err;
+}
+
