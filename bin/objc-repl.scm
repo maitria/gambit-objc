3ac33210b65ebedb7442be5a78ea818f6bd259fc
Jason Felice <jason.m.felice@gmail.com>
2013-05-17 13:43:03 -0400

    Find objc-repl in $PATH if needed

--- bin/objc-repl.scm
+++ bin/objc-repl.scm
@@ -1,8 +1,45 @@
 (include "~~lib/_gambit#.scm")
 (macro-readtable-bracket-keyword-set! ##main-readtable ':)
 
-(let* ((repl-path (path-directory (car (command-line))))
-       (objc#-path (string-append repl-path "../lib/objc#.scm")))
-  (eval `(##include ,objc#-path)))
+(define (path)
+  (define path-string (getenv "PATH"))
+  (define path-parts '())
 
+  (let loop ((start 0)
+	     (offset 0))
+    (cond
+      ((or (= offset (string-length path-string))
+	   (char=? #\: (string-ref path-string offset)))
+       (set! path-parts (cons (string-append
+				(substring path-string start offset)
+				"/")
+			      path-parts))
+       (if (= offset (string-length path-string))
+	 #f
+	 (loop (+ offset 1)
+	       (+ offset 1))))
+      (else
+       (loop start
+	     (+ offset 1)))))
+
+  (reverse path-parts))
+
+
+(define (locate-ourselves)
+  (let ((specified-directory (path-directory (car (command-line)))))
+    (cond
+      ((string=? "" specified-directory)
+       (call/cc
+	 (lambda (return)
+	   (for-each
+	     (lambda (path-part)
+	       (define possible-path (string-append path-part (car (command-line))))
+	       (if (file-exists? possible-path)
+		 (return path-part)))
+	     (path))
+	   "?/")))
+      (else
+       (path-expand specified-directory)))))
+
+(eval `(##include ,(string-append (locate-ourselves) "../lib/objc#.scm")))
 (##repl-debug-main)
