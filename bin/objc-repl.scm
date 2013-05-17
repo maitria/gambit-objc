dbd3189a83b62dac89c78cde211d58eff7ec2a81
Jason Felice <jason.m.felice@gmail.com>
2013-05-17 13:24:11 -0400

    Make objc-repl load objc#.scm

--- bin/objc-repl.scm
+++ bin/objc-repl.scm
@@ -1,4 +1,8 @@
-(include "../lib/objc#.scm")
 (include "~~lib/_gambit#.scm")
 (macro-readtable-bracket-keyword-set! ##main-readtable ':)
+
+(let* ((repl-path (path-directory (car (command-line))))
+       (objc#-path (string-append repl-path "../lib/objc#.scm")))
+  (eval `(##include ,objc#-path)))
+
 (##repl-debug-main)
